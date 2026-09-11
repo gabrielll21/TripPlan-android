80c0abf9f5149e3a2dba2349d3eac067f3d10270
Gabriel Fernandes <gabrielpaesfernandes0@gmail.com>
2026-09-11 19:06:22 -0300

    chore: adiciona dependência Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.material)
     implementation(libs.google.places)
+    implementation(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
